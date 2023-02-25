01d5544ebeabb3b0a10f2a8005c77131bd0a4d00
zbcHacknet <2206917855@qq.com>
2023-02-25 22:19:31 +0800

    注入数据源的几种方式

---
+++ itheima_spring_ioc_anno/src/test/java/com/itheima/test/DataSourceTest.java
@@ -0,0 +1,69 @@
+package com.itheima.test;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class DataSourceTest {
+    @Test
+    //测试手动Spring数据源(加载properties配置文件)
+    public void test4()throws PropertyVetoException, SQLException{
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContent.xml");
+        DataSource dataSource = app.getBean(DataSource.class);
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+    @Test
+    //测试手动创建c3p0数据源(加载properties配置文件)
+    public void test3() throws PropertyVetoException, SQLException {
+        //读取配置文件 写入配置文件名jdbc.properties
+        ResourceBundle rb = ResourceBundle.getBundle("jdbc");
+        String driver = rb.getString("jdbc.driver");
+        String url = rb.getString("jdbc.url");
+        String username = rb.getString("jdbc.username");
+        String password = rb.getString("jdbc.password");
+        //创建数据源对象 设置连接参数
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass(driver);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        Connection ccn = dataSource.getConnection();
+        System.out.println(ccn);
+        ccn.close();
+    }
+    @Test
+    //测试手动创建druid 数据源
+    public void test2() throws SQLException {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mydb?characterEncoding=utf-8&useSSL=true&serverTimezone=Asia/Shanghai");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+
+    }
+    @Test
+    //测试手动创建c3p0数据源
+    public void test1() throws PropertyVetoException, SQLException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("com.mysql.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/mydb?characterEncoding=utf-8&useSSL=true&serverTimezone=Asia/Shanghai");
+        dataSource.setUser("root");
+        dataSource.setPassword("root");
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+}
